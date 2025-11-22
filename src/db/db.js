835f296b6c897d53eae7974cd408f9cde21f90bf
dylan gimenez <gimenezdylannicolaspc@gmail.com>
2025-11-22 04:14:11 -0300

    instalo firebase

--- src/db/db.js
+++ src/db/db.js
@@ -1,21 +1,20 @@
-// Importar lo necesario
+
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; // <- ESTE FALTABA
+import { getFirestore } from "firebase/firestore"; 
 
 const firebaseConfig = {
   apiKey: "AIzaSyDqGeQLOfLeMiKNEQrQ49wDz1_T7roSmF8",
   authDomain: "coderhouse-eecomerce-dylang.firebaseapp.com",
   projectId: "coderhouse-eecomerce-dylang",
-  storageBucket: "coderhouse-eecomerce-dylang.appspot.com", // <- CORREGIDO
+  storageBucket: "coderhouse-eecomerce-dylang.appspot.com", 
   messagingSenderId: "511932505570",
   appId: "1:511932505570:web:3a6c9a0455366d95d6edbb",
   measurementId: "G-KLLM5VBJTQ",
 };
 
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 
-// Inicializar Firestore (pasando la app)
+
 const db = getFirestore(app);
 
 export default db;
